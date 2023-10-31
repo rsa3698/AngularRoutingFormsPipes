e2b9b146537d998e6cfcd18d5fc45d6770d87792
Raudra SA <raudra.sankar.adak@accenture.com>
2023-10-31 14:46:22 +0530

    Initial Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { RolesComponent } from './roles/roles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    CategoriesComponent,
+    RolesComponent
   ],
   imports: [
     BrowserModule
